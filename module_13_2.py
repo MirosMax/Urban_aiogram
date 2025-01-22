a329f86585c08e3b21945d1f7b2593b2cba8108d
Max Miroshnichenko <foxmax@list.ru>
2025-01-23 01:02:25 +0300

    Initial

---
+++ module_13_2.py
@@ -0,0 +1,23 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+with open('api.txt', 'r', encoding='utf-8') as file_api:
+    api = file_api.read()
+
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_message(message):
+    print('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
